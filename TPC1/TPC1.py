3871b55391c2ae1cd315d9bdfc50cd596573feec
João Cardo <100542887+JBB-DO-MAL-V2@users.noreply.github.com>
2023-03-29 17:26:10 +0000

    Update TPC1.py

--- TPC1/TPC1.py
+++ TPC1/TPC1.py
@@ -1,6 +1,6 @@
 import csv
 from prettytable import PrettyTable
-
+import matplotlib.pyplot as plt
 
 #List of dictionaries
 lista = []
@@ -36,7 +36,6 @@ def distribution_by_age(lista):
 		if (int(dicionario['idade']) >= 40 and int(dicionario['idade']) <= 44):
 			dict_por_idade['[40-44]'] += 1
 
-	
 
 	return dict_por_idade
 
@@ -46,18 +45,12 @@ def distribution_by_age(lista):
 
 def distribution_by_colesterol(lista):
 	dict_por_colesterol = {}
-
-
-
 	for i in range(0, 604, 10):
 		dict_por_colesterol[str(i) + '-' + str(i+10)] = 0 #{'0-10': 0, '10-20': 0, '20-30': 0....}
-
-
 	for i in range(0, 604, 10):
 		for dicionario in lista:
 			if(int(dicionario['colesterol']) >= i and int(dicionario['colesterol']) < i+10):
 				dict_por_colesterol[str(i) + '-' + str(i+10)] += 1
-
 
 	return dict_por_colesterol
 
@@ -73,6 +66,33 @@ def pretty_print(dicionario):
 
 
 
+def plot_distribution_by_age(lista):
+    dict_por_idade = distribution_by_age(lista)
+    labels = dict_por_idade.keys()
+    sizes = dict_por_idade.values()
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+    plt.title('Distribution by age')
+    plt.show()
+
+
+def plot_distribution_by_sex(lista):
+    dict_por_sexo = distribution_by_sex(lista)
+    plt.bar(dict_por_sexo.keys(), dict_por_sexo.values())
+    plt.xlabel('Sex')
+    plt.ylabel('Count')
+    plt.title('Distribution by sex')
+    plt.show()
+
+def plot_distribution_by_colesterol(lista):
+    dict_por_colesterol = distribution_by_colesterol(lista)
+    x = list(dict_por_colesterol.keys())
+    y = list(dict_por_colesterol.values())
+    plt.hist(x, weights=y, bins=len(x))
+    plt.xlabel('Colesterol interval')
+    plt.ylabel('Count')
+    plt.title('Distribution by colesterol')
+    plt.show()
+
 
 if __name__ == '__main__':
 	#print(lista)
@@ -82,4 +102,9 @@ if __name__ == '__main__':
 	pretty_print(distribution_by_age(lista)) 
 	print("Por colesterol, separado por intervalos de 10\n")
 	pretty_print(distribution_by_colesterol(lista)) 
+	plot_distribution_by_age(lista)
+	plot_distribution_by_sex(lista)
+	plot_distribution_by_colesterol(lista)
+
+
 
